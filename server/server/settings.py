7f116ed6b5b575a7ea41df0a298fc4e137d8403c
mango-shaved-ice <mango-shaved-ice@users.noreply.github.com>
2018-09-17 18:29:05 -0700

    Added Article and Comment models

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'userdata.apps.UserdataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- server/userdata/models.py
+++ server/userdata/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    id = models.CharField(max_length=30, db_index=True, unique=True, primary_key=True)
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    published = models.DateTimeField()
+    ip = models.GenericIPAddressField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.CharField(max_length=30)
+    content = models.TextField()
+    published = models.DateTimeField()
+    ip = models.GenericIPAddressField()
+    TAG_TYPES = (('PUSH', 'push'), ('NEUTRAL', 'neutral'), ('BOO', 'boo'))
+    tag = models.CharField(max_length=10, choices=TAG_TYPES)
